4d88f66bb4feed4e5404a11b6279f50645b2677a
Heetesh Panghanti <Heetesh.Panghanti@wolterskluwer.com>
2023-04-26 15:50:29 +0530

    Implement an API endpoint using .NET that retrieves the details of a specific launch

---
+++ DotNetWebAPI/DotNetWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using DotNetWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Net.Http;
+
+namespace DotNetWebAPI.Controllers
+{
+	[ApiController]
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly IHttpClientFactory _httpClientFactory;
+		private readonly IConfiguration _configuration;
+
+
+		public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClient, IConfiguration Configuration)
+		{
+			_logger = logger;
+			_httpClientFactory= httpClient;
+			_configuration = Configuration;
+		}
+
+		[HttpGet]
+		[Route("/Launch/{id}")]
+		[ResponseCache(Duration = 30, Location = ResponseCacheLocation.Any, NoStore = true)]
+		public Task<HttpResponseMessage> Launch(int id) {
+			if (id <= 0)
+			{
+				throw new Exception("Id must be greater then 0!");
+			}
+
+			return _httpClientFactory.CreateClient().GetAsync($"{_configuration.GetSection("EndpointUrl").Value}launches/{id}");
+		}
+	}
+}
